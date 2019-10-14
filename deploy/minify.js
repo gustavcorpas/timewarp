28f5832dedc9df916b966804467c129743f96acc
Anton Juul-Naber <anton@juul-naber.dk>
2019-10-14 19:09:10 +0200

    Update minify.js

--- deploy/minify.js
+++ deploy/minify.js
@@ -4,22 +4,33 @@ const path = require('path');
 const minify = require("minify");
 
 const site_path = "/home/travis/build/antonjuulnaber/timewarp";
+const site_dirs = ["/", "/css", "/js", "/data"];
 
 function fail(e){
 	console.error(e);
 	process.exit(1);
 }
 
+let filesList;
+
+for(const dir of site_dirs){
+	fs.stat(site_path + dir, e => {
+		if(!e){
+			
+			fs.readdir(site_path + dir, (e, files) => {
+				filesList = files.filter(function(e){
+					return path.extname(e).toLowerCase() === ".html" || ".css" || ".js";
+				});
+				console.log(filesList);
+			});
+			
+			
+			
+		}
+	});
+}
+
+
 minify("index.html").then(file => {
 	fs.writeFile("index.html", file, e => {if(e) fail(e);});
 }).catch(e => {fail(e);});
-
-
-let filesList;
-fs.readdir(site_path, function(err, files){
-  console.log(files);
-  filesList = files.filter(function(e){
-    return path.extname(e).toLowerCase() === '.html'
-  });
-  console.log(filesList);
-});
