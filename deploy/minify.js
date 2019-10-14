0623bc2ada8b529122ea03a803f08855ed9e6308
Anton Juul-Naber <anton@juul-naber.dk>
2019-10-14 18:29:52 +0200

    Update minify.js

--- deploy/minify.js
+++ deploy/minify.js
@@ -11,4 +11,12 @@ minify("index.html").then(file => {
 	fs.writeFile("index.html", "VIRK DOG", e => {if(e) fail(e);});
 }).catch(e => {fail(e);});
 
-fs.writeFile("index.html", "VIRK DOG", e => {if(e) fail(e);});
+const content = "Jeg er sej";
+
+fs.writeFile('/index.html', content, err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log("File written succesfully");
+});
