cebff8d7ab6d66752cac7b9ce47228e43669a73f
Anton Juul-Naber <anton@juul-naber.dk>
2019-10-14 18:45:05 +0200

    Update minify.js

--- deploy/minify.js
+++ deploy/minify.js
@@ -1,5 +1,6 @@
 "use strict";
 const fs = require("fs");
+var path = require('path');
 const minify = require("minify");
 
 function fail(e){
@@ -10,3 +11,12 @@ function fail(e){
 minify("index.html").then(file => {
 	fs.writeFile("index.html", file, e => {if(e) fail(e);});
 }).catch(e => {fail(e);});
+
+
+let filesList;
+fs.readdir("", function(err, files){
+  filesList = files.filter(function(e){
+    return path.extname(e).toLowerCase() === '.html'
+  });
+  console.log(filesList);
+});
