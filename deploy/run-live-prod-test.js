0cafade6cbe248fb4680a0d4c2e682dcdb3ecc4f
Anton Juul-Naber <anton@juul-naber.dk>
2019-10-15 04:32:39 +0200

    Improved deploy

--- deploy/run-live-prod-test.js
+++ deploy/run-live-prod-test.js
@@ -1,6 +1,7 @@
 "use strict";
 const site_root = "/home/travis/build/antonjuulnaber/timewarp/";
 
+const console = require(site_root + "deploy/console.js");
 const live = require(site_root + "deploy/live-test.js");
 
 live.test("https://timewarp.antonjuulnaber.dk");
